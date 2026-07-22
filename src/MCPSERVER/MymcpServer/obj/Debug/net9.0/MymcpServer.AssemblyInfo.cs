ce76c6a427a1f1cec12344bb25add9d9fa903a28
MOSHO17 <mohitparulekar17@gmail.com>
2026-07-22 17:51:53 +0530

    made changes to EMailtools

--- src/MCPSERVER/MymcpServer/obj/Debug/net9.0/MymcpServer.AssemblyInfo.cs
+++ src/MCPSERVER/MymcpServer/obj/Debug/net9.0/MymcpServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MymcpServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40dea163519ff653ba0dc1124819ea12181c54fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e013d3c4ec0be465ee675ef59f7354796ec6db8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MymcpServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MymcpServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
